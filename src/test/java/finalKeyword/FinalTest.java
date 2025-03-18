ede4605f1aadfcd3c55055ab8e246a12ae2365db
LeventeCornea <cornealevente13@gmail.com>
2025-03-18 16:29:33 +0200

    Am invatat despre static

---
+++ src/test/java/finalKeyword/FinalTest.java
@@ -0,0 +1,18 @@
+package finalKeyword;
+
+import org.testng.annotations.Test;
+
+public class FinalTest {
+
+    //Variabila = proprietate care poate sa-si modifica valoarea
+    //Constanta = proprietate care nu poate sa-si modifica valoarea
+    //Scopul unei constante poate sa fie acela de a pastra aceleasi valori in intreg proiectul
+
+    public String user = "Andrei@gmail.com";
+    public String password = "123456";
+
+    @Test
+    public void loginTest(){
+        System.out.println("Userul s-a logat cu succes folosing user: " + user + " gitsi parola " + password);
+    }
+}
