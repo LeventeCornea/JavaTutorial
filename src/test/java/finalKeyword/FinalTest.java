fa6c2b7e1c333d36018ff912fc65a5f337f29c0a
LeventeCornea <cornealevente13@gmail.com>
2025-03-18 17:17:38 +0200

    Am terminat cu final

--- src/test/java/finalKeyword/FinalTest.java
+++ src/test/java/finalKeyword/FinalTest.java
@@ -13,17 +13,6 @@ public class FinalTest {
 
     @Test
     public void loginTest(){
-        System.out.println("Userul s-a logat cu succes folosing user: " + user + " gitsi parola " + password);
-    //Variabila = proprietate care isi poate modifica valoarea
-    //Constanta = proprietate care nu isi poate modifica valoarea
-    //Scopul unei constante poate sa fie acela de a pastra aceleasi valori in intreg proiectul
-
-    public final String user = "AdrianNastase";
-    public final String password = "jelszo";
-
-    @Test
-    public void loginTest(){
-        System.out.println("Userul s-a logat cu succes folosind user: " + user + " si parola: " + password);
->>>>>>> FinalKeywordBranch
+        System.out.println("Userul s-a logat cu succes folosing user: " + user + " parola " + password);
     }
 }
